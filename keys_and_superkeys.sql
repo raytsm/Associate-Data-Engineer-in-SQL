e1dd846f8507085618af9b1b2ae756b53db25a69
Roshan Thapa <roshanrays236@gmail.com>
2026-06-24 18:56:40 +0545

    Completed

--- keys_and_superkeys.sql
+++ keys_and_superkeys.sql
@@ -19,7 +19,7 @@ CREATE TABLE products (
     price numberic
 );
 
--- Designmate composite key as primary key
+-- Designate composite key as primary key
 CREATE TABLE example (
     a integer,
     b integer,
@@ -29,3 +29,66 @@ CREATE TABLE example (
 
 ALTER TABLE table_name
 ADD CONSTRAINT some_name PRIMARY KEY (column_name)
+
+ALTER TABLE car 
+ADD COLUMN id serial PRIMARY KEY;
+
+ALTER TABLE table_name
+ADD COLUMN column_c varchar(256);
+
+UPDATE TABLE table_name
+SET column_c = CONCAT(column_a, column_b);
+
+ALTER TABLE table_name
+ADD CONSTRAINT pk PRIMARY KEY (column_c);
+
+CREATE TABLE manufacturers (
+    name varchar(255) PRIMARY KEY
+);
+
+INSERT INTO manufacturers
+VALUES ('Ford'), ('VW'), ('GM')
+
+CREATE TABLE cars (
+    model varchar(255) PRIMARY KEY,
+    manufacturer_name varchar(255) REFERENCES manufacturers (name) -- or separate column and relatinoship definition as manufacturer_name varchar(255), FOREIGN KEY (manufacturer_name) REFERENCES manufacturers (name)
+);
+
+INSERT INTO cars
+VALUES ('Ranger', 'Ford'), ('Beetle', 'VW');
+
+--Throws an error!
+INSERT INTO cars
+VALUES ('Tundra', 'Toyota');
+
+ALTER TABLE a
+ADD CONSTRAINT a_fkey FOREIGN KEY (b_id) REFERENCES b (id);
+
+CREATE TABLE affiliations (
+    professor_id integer REFERENCES professors (id),
+    organization_id varchar(256) REFERENCES organizations (id),
+    function varchar(256)
+)
+
+CREATE TABLE a (
+    id integer PRIMARY KEY,
+    column_a varchar(64),
+    ...,
+    b_id integer REFERENCES b (id) ON DELETE NO ACTION
+)
+
+CREATE TABLE a (
+    id integer PRIMARY KEY,
+    column_a varchar(64),
+    ...,
+    b_id integer REFERENCES b (id) ON DELETE CASCADE
+)
+
+--Identify the correct constraint name
+SELECT constraint_name, table_name, constraint_type
+FROM information_schema.table_constraints
+WHERE constraint_type = 'FOREIGN KEY';
+
+--Drop the right foreign key constraint
+ALTER TABLE affiliations
+DROP CONSTRAINT constraint_name;
